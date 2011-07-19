0e0595c22e4f79d41a22a55075a0c6539927f742
Francesco Paolo Lovergine <frankie@debian.org>
2011-07-19 10:08:22 +0200

    This is the current lenny branch.

---
+++ mapswf.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * $Id: mapswf.h 6108 2007-05-10 19:47:54Z warmerdam $
+ *
+ * Project:  MapServer
+ * Purpose:  header file for Ming support (swf flash output) in Mapserver
+ * Author:   Assefa Yewondwossen (yassefa@dmsolutions.ca)
+ ****************************************************************************/
+
+
+#ifdef USE_MING_FLASH
+#include "ming.h"
+#endif
+
+
+/* SWF Object structure */
+#ifdef USE_MING_FLASH
+   
+typedef struct  {
+  mapObj *map;
+  SWFMovie sMainMovie;
+  int nLayerMovies;
+  SWFMovie *pasMovies;
+  int nCurrentMovie;
+  int nCurrentLayerIdx;
+  int nCurrentShapeIdx;
+  
+  
+  void    *imagetmp;  /*used when the output format is SINGLE */
+                      /*(one movie for the whole map)*/
+  int *panLayerIndex; /* keeps the layer index for every movie created. */
+} SWFObj;
+#endif
